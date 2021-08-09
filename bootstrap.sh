0e78c6465e8d7f4d42e88cd12e786e24251ecc3e
JS2021 <saviorofauir@gmail.com>
2021-08-09 11:29:28 -0500

    bootstrap.sh: set working directory

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+# Set working directory to 
+cd "$(dirname "$0")"
+
 # Install missing packages
 sudo apt-get update && sudo apt-get install -y git
 
 # Move dotfiles to user's home folder
 echo 'Moving dotfiles...'
-cp -r debian-bootstrap/dotfiles/  ~/
+cp -r dotfiles/.  ~/
 echo 'Moving dotfiles completed.'
