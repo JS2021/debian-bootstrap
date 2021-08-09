1979eaaeaae4403d48677c38083761a8faac640b
JS2021 <saviorofauir@gmail.com>
2021-08-09 08:57:49 -0500

    'Updated bootstrap.sh'

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-sudo apt-get install -y git
+# Install missing packages
+format='${binary:Package}\t${Version}\t${Architecture}\t${binary:Summary}\n'
+dpkg-query -W -f=$format git || sudo apt-get install git
+dpkg-query -W -f=$format wget || sudo apt-get install wget
+
 # Move dotfiles to user's home folder
-echo 'Moving dotfiles'
+echo 'Moving dotfiles...'
 cp -r dotfiles/  test-home-folder/
-# Move dotfiles
-echo "Moving dotfiles completed."
-
-
-#./configs/move-configs.sh
-#./dotfiles/move-dotfiles.sh
-#./scripts/move-scripts.sh
+echo 'Moving dotfiles completed.'
