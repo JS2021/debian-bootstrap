ae66374e18e73266c286b67460835a23a8735da7
JS2021 <saviorofauir@gmail.com>
2021-08-09 11:02:07 -0500

    bootstrap.sh: add apt update

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,9 +3,6 @@
 # Install missing packages
 sudo apt-get update && sudo apt-get install -y git
 
-# Clone repository
-git clone https://github.com/JS2021/debian-bootstrap.git
-
 # Move dotfiles to user's home folder
 echo 'Moving dotfiles...'
 cp -r debian-bootstrap/dotfiles/  ~/
